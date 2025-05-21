ad35e9989a2f3e5d5d389f69634480c3799c4411
Armando Alamo <armandoalamo@seti.com.co>
2025-05-21 11:22:27 -0500

    Update AuthController.cs

--- APIProjectBackend/Controllers/AuthController.cs
+++ APIProjectBackend/Controllers/AuthController.cs
@@ -6,7 +6,7 @@ using APIProjectBackend.Service.Contracts;
 namespace APIProjectBackend.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
